bb1ed113a7c198ffe014eba2728d53f6dbcc47d8
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-28 13:57:53 +0100

    root commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarEventosComponent } from './components/listar-eventos/listar-eventos.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { EventoServiceService } from './evento-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarEventosComponent, DeleteDialogComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    MatDialogModule,
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(), HttpClient, EventoServiceService, 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
